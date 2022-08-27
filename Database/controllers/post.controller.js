382c7e49a2f9c7bec428351b2a7aca2f1bb4f1fb
atemcozz <atemcozzartem@gmail.com>
2022-08-27 12:16:39 +0300

    .

--- Database/controllers/post.controller.js
+++ Database/controllers/post.controller.js
@@ -1,3 +1,4 @@
+const tokenService = require("../../service/token-service");
 const db = require("../db");
 
 class PostController {
@@ -48,44 +49,33 @@ class PostController {
     res.json({ ...post, user, likesCount, commentsCount, attachments });
   }
   async getPosts(req, res) {
-    const posts = (
-      await db.query(`select * from post order by created_at desc`)
-    ).rows;
-
-    const postsData = await Promise.all(
-      posts.map(async ({ user_id, ...post }) => {
-        const attachments = (
-          await db.query(`select type, url from post_media where post_id=$1`, [
-            post.id,
-          ])
-        ).rows;
-        const user = (
-          await db.query(
-            `select id, name,surname,nickname,avatar_url from person where id=$1`,
-            [user_id]
-          )
-        ).rows[0];
-        const likesCount = (
-          await db.query(`select count(*) from post_like where post_id=$1`, [
-            post.id,
-          ])
-        ).rows[0].count;
-        const commentsCount = (
-          await db.query(`select count(*) from comment where post_id=$1`, [
-            post.id,
-          ])
-        ).rows[0].count;
-
-        return {
-          ...post,
-          user,
-          likesCount,
-          commentsCount,
-          attachments,
-        };
-      })
+    const user = tokenService.validateAccessToken(
+      req.headers.authorization.split(" ")[1]
     );
-    res.json(postsData);
+    const posts = (
+      await db.query(`SELECT p.id,p.description,p.created_at,
+      to_jsonb(u.*) - 'passwordhash' AS USER,
+      array_agg(to_jsonb(pm.*) - 'id' - 'post_id') AS attachments,
+      (SELECT count(*) FROM post_like WHERE post_id = p.id ) AS likes_count,
+      (SELECT count(*) FROM COMMENT WHERE post_id = p.id ) AS comments_count
+      FROM post p
+      JOIN person u ON p.user_id = u.id
+      LEFT JOIN post_media pm ON p.id = pm.post_id
+      GROUP BY p.id, u.id
+      ORDER BY p.created_at DESC`)
+    ).rows;
+    if (user) {
+      for (const post of posts) {
+        post.userLike =
+          (
+            await db.query(
+              "select count(*) from post_like where user_id=$1 and post_id=$2",
+              [user.id, post.id]
+            )
+          ).rows[0].count > 0;
+      }
+    }
+    res.json(posts);
   }
   async deletePost(req, res) {
     const id = req.params.id;
@@ -113,22 +103,6 @@ class PostController {
     }
     res.status(200).end();
   }
-  async getLikeStatus(req, res) {
-    const { user_id, post_id } = req.query;
-    if (user_id === "undefined" || post_id === "undefined") {
-      return res.status(404).end();
-    }
-    const liked = (
-      await db.query(
-        "select * from post_like where user_id=$1 and post_id=$2",
-        [user_id, post_id]
-      )
-    ).rows[0];
-    if (liked) {
-      return res.status(200).json({ liked: true });
-    }
-    res.status(200).json({ liked: false });
-  }
 
   async test(req, res) {
     const query = `select
@@ -152,7 +126,7 @@ class PostController {
       left join post_like pl on p.id = pl.post_id
     group by
       p.id,
-      u.id;`;
+      u.id`;
     const testres = (await db.query(query)).rows;
     res.json(testres);
   }
--- Database/routes/post.routes.js
+++ Database/routes/post.routes.js
@@ -4,7 +4,6 @@ const postController = require("../controllers/post.controller");
 const authMiddleware = require("../../middleware/authMiddleware");
 router.post("/post", postController.createPost);
 router.post("/like_post", authMiddleware, postController.likePost);
-router.get("/post_liked", postController.getLikeStatus);
 router.get("/posts", postController.getPosts);
 router.get("/post/:id", postController.getOnePost);
 router.get("/posts/user", postController.getPostsByUser);
