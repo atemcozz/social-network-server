bf357e833a8d41da67616ffb00a15a9acda75367
atemcozz <atemcozzartem@gmail.com>
2022-08-31 18:25:07 +0300

    .

--- Database/db.js
+++ Database/db.js
@@ -1,11 +1,13 @@
 const pg = require("pg");
-
+require("dotenv").config();
+// const pool = new pg.Pool({
+//   user: "postgres",
+//   password: "S0602k",
+//   host: "localhost",
+//   port: 5432,
+//   database: "social_network",
+// });
 const pool = new pg.Pool({
-  user: "postgres",
-  password: "S0602k",
-  host: "localhost",
-  port: 5432,
-  database: "social_network",
+  connectionString: process.env.DATABASE_URL,
 });
-
 module.exports = pool;
